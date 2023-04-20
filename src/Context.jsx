f7958a60f8587a47d722eef3398fc67d4f4e3c04
Raymond <raymondurrutia2@yahoo.com>
2023-04-20 11:27:02 -0400

    Added functionality to add images to cart, change plus icon to full shopping cart icon when an image is added to cart, being able to remove item from cart, and uopdate header cart icon to full cart icon when there's an item in the cart

--- src/Context.jsx
+++ src/Context.jsx
@@ -5,10 +5,12 @@ const Context = React.createContext()
 function ContextProvider({children}) {
 
     const [allPhotos, setAllPhotos] = useState([])
+    const [cartItems, setCartItems] = useState([])
 
     // Images API
     const url = "https://raw.githubusercontent.com/bobziroll/scrimba-react-bootcamp-images/master/images.json"
 
+    // Get the images
     useEffect(() => {
         fetch(url)
             .then(res => res.json())
@@ -20,14 +22,25 @@ function ContextProvider({children}) {
             if (photo.id === id) {
                 return { ...photo, isFavorite: !photo.isFavorite }
             }
+
             return photo
         })
 
         setAllPhotos(updatedArr)
     }
 
+    function addToCart(newItem) {
+        setCartItems((prevItems) => [ ...prevItems, newItem])
+        console.log(newItem.id)
+    }
+
+    function removeFromCart(id) {
+        // Get the previous cart items and filter out the cart items that don't match the items id
+        setCartItems((prevItems) => prevItems.filter((item) => item.id != id))
+    }
+
     return (
-        <Context.Provider value={{allPhotos, toggleFavorite}}>
+        <Context.Provider value={{allPhotos, toggleFavorite, addToCart, cartItems, removeFromCart}}>
             {children}
         </Context.Provider>
     )
--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -4,23 +4,32 @@ import {Context} from "../Context"
 
 function Image({className, img}) {
 
-    const {toggleFavorite} = useContext(Context)
+    const {toggleFavorite, addToCart, cartItems, removeFromCart} = useContext(Context)
 
     const [hovered, setHovered] = useState(false)
 
     function heartIcon() {
         // If the image is favorited, show the filled heart icon
-        // If it's not favorited and it's being hovered over, show the unfilled heart icon
         if (img.isFavorite) {
             return <i className="ri-heart-fill favorite" onClick={() => toggleFavorite(img.id)}></i>
+        // If it's not favorited and it's being hovered over, show the unfilled heart icon
         } else if (hovered) {
             return <i className="ri-heart-line favorite" onClick={() => toggleFavorite(img.id)}></i>
         }
     }
 
+    function cartIcon() {
+        // Check if the item's id matches any of the image id's in the cart
+        const itemIsAlreadyInCart = cartItems.some((item) => item.id === img.id)
 
-    const cartIcon = hovered && <i className="ri-add-circle-line cart"></i>
-
+        // If the item is already in the cart show the filled shopping cart icon
+        if (itemIsAlreadyInCart) {
+            return <i className="ri-shopping-cart-fill cart" onClick={() => removeFromCart(img.id)}></i>
+        // If the image is being hovered show the unfilled shopping cart icon
+        } else if (hovered) {
+            return <i className="ri-add-circle-line cart" onClick={() => addToCart(img)}></i>
+        } 
+    }
 
     return (
         <div 
@@ -30,12 +39,12 @@ function Image({className, img}) {
         >
             <img src={img.url} className="image-grid" />
             {heartIcon()}
-            {cartIcon}
+            {cartIcon()}
         </div>
     )
 }
 
-image.propTypes = {
+Image.propTypes = {
     className: PropTypes.string,
     img: PropTypes.shape({
         id: PropTypes.string.isRequired,
